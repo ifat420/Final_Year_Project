2c37cddd76b7ef15a287d2fdfe0024df2b8bb9b6
Iftakhar Ahmed <ifat802@gmail.com>
2018-02-18 01:05:44 +0600

    a lot updates
    both File

--- sql/constrain.sql
+++ sql/constrain.sql
@@ -1,44 +1,137 @@
 /* indexes for TABLE faculty */
-ALTER TABLE faculty ADD CONSTRAINT fk_faculty PRIMARY KEY(faculty_id);
+ALTER TABLE faculty ADD CONSTRAINT pk_faculty PRIMARY KEY(faculty_id);
 ALTER TABLE faculty ADD CONSTRAINT faculty_unique UNIQUE(faculty_name);
-
-/* indexes for TABLE department */
-ALTER TABLE department ADD CONSTRAINT fk_department PRIMARY KEY(department_id);
-ALTER TABLE department ADD CONSTRAINT department_name_unique UNIQUE(department_name);
-ALTER TABLE department ADD CONSTRAINT department_abbr_unique UNIQUE(department_abbr);
-ALTER TABLE department ADD CONSTRAINT department_code_unique UNIQUE(department_code);
-
-/* indexes for TABLE program */
-ALTER TABLE program ADD CONSTRAINT fk_program PRIMARY KEY(program_id);
-ALTER TABLE program ADD CONSTRAINT program_name_unique UNIQUE(program_name);
-ALTER TABLE program ADD CONSTRAINT program_abbr_unique UNIQUE(program_abbr);
-ALTER TABLE program ADD CONSTRAINT program_degree_unique UNIQUE(degree);
-
-/* indexes for TABLE session_ */
-ALTER TABLE session_ ADD CONSTRAINT fk_session_ PRIMARY KEY(session_id);
-ALTER TABLE session_ ADD CONSTRAINT session_desc_unique UNIQUE(session_desc);
-ALTER TABLE session_ ADD CONSTRAINT academic_year_unique UNIQUE(academic_year);
-
-/* indexes for TABLE semester */
-ALTER TABLE semester ADD CONSTRAINT fk_semester PRIMARY KEY(semester_id);
-
-/* indexes for TABLE residential_ */
-ALTER TABLE residential_hall ADD CONSTRAINT fk_residential PRIMARY KEY(residential_id);
-ALTER TABLE residential_hall ADD CONSTRAINT residential_name_unique UNIQUE(residential_name);
-
-/* indexes for TABLE student_info */
-ALTER TABLE student_info ADD CONSTRAINT fk_student PRIMARY KEY(student_roll);
-ALTER TABLE student_info ADD CONSTRAINT registration_id_unique UNIQUE(registration_id);
-
-/* indexes for TABLE courses */
-ALTER TABLE courses ADD CONSTRAINT fk_course PRIMARY KEY(course_id);
-ALTER TABLE courses ADD CONSTRAINT course_code_unique UNIQUE(course_code);
-ALTER TABLE courses ADD CONSTRAINT course_title_unique UNIQUE(course_title);
-
-/* indexes for TABLE promotion */
-ALTER TABLE promotion ADD CONSTRAINT fk_promotion PRIMARY KEY(promotion_id);
 
 /* indexes for TABLE teacher */
 ALTER TABLE teacher ADD CONSTRAINT fk_teacher PRIMARY KEY(teacher_id);
 
+
+/* indexes for TABLE department */
+ALTER TABLE department ADD CONSTRAINT pk_department PRIMARY KEY(department_id);
+ALTER TABLE department ADD CONSTRAINT department_name_unique UNIQUE(department_name);
+ALTER TABLE department ADD CONSTRAINT department_abbr_unique UNIQUE(department_abbr);
+ALTER TABLE department ADD CONSTRAINT department_code_unique UNIQUE(department_code);
+ALTER TABLE department
+ADD CONSTRAINT fk_dept_faculty
+  FOREIGN KEY (faculty_id)
+  REFERENCES faculty(faculty_id);
+
+/* indexes for TABLE program */
+ALTER TABLE program ADD CONSTRAINT pk_program PRIMARY KEY(program_id);
+ALTER TABLE program ADD CONSTRAINT program_name_unique UNIQUE(program_name);
+ALTER TABLE program ADD CONSTRAINT program_abbr_unique UNIQUE(program_abbr);
+ALTER TABLE program ADD CONSTRAINT program_degree_unique UNIQUE(degree);
+ALTER TABLE program
+ADD CONSTRAINT fk_pro_department
+  FOREIGN KEY (department_id)
+  REFERENCES department(department_id);
+
+/* indexes for TABLE session_ */
+ALTER TABLE session_ ADD CONSTRAINT pk_session_ PRIMARY KEY(session_id);
+ALTER TABLE session_ ADD CONSTRAINT session_desc_unique UNIQUE(session_desc);
+ALTER TABLE session_ ADD CONSTRAINT academic_year_unique UNIQUE(academic_year);
+ALTER TABLE session_
+ADD CONSTRAINT fk_sec_program
+  FOREIGN KEY (program_id)
+  REFERENCES program(program_id);
+
+
+/* indexes for TABLE semester */
+ALTER TABLE semester ADD CONSTRAINT pk_semester PRIMARY KEY(semester_id);
+
+/* indexes for TABLE residential_ */
+ALTER TABLE residential_hall ADD CONSTRAINT pk_residential PRIMARY KEY(residential_id);
+ALTER TABLE residential_hall ADD CONSTRAINT residential_name_unique UNIQUE(residential_name);
+
+/* indexes for TABLE student_info */
+ALTER TABLE student_info ADD CONSTRAINT pk_student PRIMARY KEY(student_roll);
+ALTER TABLE student_info ADD CONSTRAINT registration_id_unique UNIQUE(registration_id);
+ALTER TABLE student_info
+ADD CONSTRAINT fk_stu_session_
+  FOREIGN KEY (session_id)
+  REFERENCES session_(session_id);
+  
+ALTER TABLE student_info
+ADD CONSTRAINT fk_stu_residential_hall
+  FOREIGN KEY (residential_id)
+  REFERENCES residential_hall(residential_id);
+
+
+/* indexes for TABLE courses */
+ALTER TABLE courses ADD CONSTRAINT pk_course PRIMARY KEY(course_id);
+ALTER TABLE courses ADD CONSTRAINT course_code_unique UNIQUE(course_code);
+ALTER TABLE courses ADD CONSTRAINT course_title_unique UNIQUE(course_title);
+
+ALTER TABLE courses
+ADD CONSTRAINT fk_cor_semester
+  FOREIGN KEY (semester_id)
+  REFERENCES semester(semester_id);
+  
+ALTER TABLE courses
+ADD CONSTRAINT fk_cor_session_
+  FOREIGN KEY (session_id)
+  REFERENCES session_(session_id);
+
+ALTER TABLE courses
+ADD CONSTRAINT fk_cor_teacher
+  FOREIGN KEY (teacher_id)
+  REFERENCES teacher(teacher_id);
+
+
+/* indexes for TABLE promotion */
+ALTER TABLE promotion ADD CONSTRAINT fk_promotion PRIMARY KEY(promotion_id);
+
+ALTER TABLE promotion
+ADD CONSTRAINT fk_pro_semester
+  FOREIGN KEY (semester_id)
+  REFERENCES semester(semester_id);
+  
+ALTER TABLE promotion
+ADD CONSTRAINT fk_pro_session_
+  FOREIGN KEY (session_id)
+  REFERENCES session_(session_id);
+  
+/* indexes for TABLE marks_theory */
+ALTER TABLE marks_theory
+ADD CONSTRAINT fk_mt_course
+  FOREIGN KEY (course_id)
+  REFERENCES courses(course_id);
+  
+ALTER TABLE marks_theory
+ADD CONSTRAINT fk_mt_student_info
+  FOREIGN KEY (student_roll)
+  REFERENCES student_info(student_roll);
+  
+/* indexes for TABLE marks_lab */
+ALTER TABLE marks_lab
+ADD CONSTRAINT fk_ml_course
+  FOREIGN KEY (course_id)
+  REFERENCES courses(course_id);
+  
+ALTER TABLE marks_lab
+ADD CONSTRAINT fk_ml_student_info
+  FOREIGN KEY (student_roll)
+  REFERENCES student_info(student_roll);
+  
+/* indexes for TABLE marks_viva */
+ALTER TABLE marks_viva
+ADD CONSTRAINT fk_mv_course
+  FOREIGN KEY (course_id)
+  REFERENCES courses(course_id);
+  
+ALTER TABLE marks_viva
+ADD CONSTRAINT fk_mv_student_info
+  FOREIGN KEY (student_roll)
+  REFERENCES student_info(student_roll);
+  
+/* indexes for TABLE marks_viva */
+ALTER TABLE marks_thesis_or_project
+ADD CONSTRAINT fk_tp_course
+  FOREIGN KEY (course_id)
+  REFERENCES courses(course_id);
+  
+ALTER TABLE marks_thesis_or_project
+ADD CONSTRAINT fk_tp_student_info
+  FOREIGN KEY (student_roll)
+  REFERENCES student_info(student_roll);
 
--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,109 +1,134 @@
 CREATE TABLE faculty(
-  faculty_id       NUMBER(2)   NOT NULL,
+  faculty_id       VARCHAR(2)   NOT NULL,
   faculty_name     VARCHAR(60) NOT NULL 
 );
 
 CREATE TABLE department(
-  department_id      NUMBER(2)   NOT NULL,
+  department_id      VARCHAR(2)   NOT NULL,
   department_name    VARCHAR(60) NOT NULL,
   department_abbr    VARCHAR(10) NOT NULL,
-  department_code    NUMBER(10)  NOT NULL
+  department_code    VARCHAR(10)  NOT NULL,
+  faculty_id         VARCHAR(2)
 );
 
 CREATE TABLE program(
-  program_id      NUMBER(10)  NOT NULL,
+  program_id      VARCHAR(10) NOT NULL,
   program_name    VARCHAR(60) NOT NULL,
   program_abbr    VARCHAR(10) NOT NULL,
-  degree       VARCHAR(10)    NOT NULL
+  degree          VARCHAR(10) NOT NULL,
+  department_id   VARCHAR(2)
+
 );
 
 CREATE TABLE session_(
-  session_id      NUMBER(10)   NOT NULL,
-  session_desc    VARCHAR(60)  NOT NULL,
-  academic_year   DATE   NOT NULL
+  session_id            VARCHAR(10)   NOT NULL,
+  session_id_origin     VARCHAR(10),
+  session_desc          VARCHAR(60)  NOT NULL,
+  academic_year         DATE   NOT NULL,
+  program_id            VARCHAR(10)
 );
 
 CREATE TABLE semester(
-  semester_id NUMBER(10)   NOT NULL,
+  semester_id VARCHAR(10)   NOT NULL,
   year        DATE   NOT NULL,
-  sem         NUMBER(3)    NOT NULL
+  sem         FLOAT(5)    NOT NULL
 );
 
 CREATE TABLE residential_hall(
-  residential_id        NUMBER(10)  NOT NULL,
-  residential_type      VARCHAR(10) NOT NULL,
+  residential_id        VARCHAR(10)  NOT NULL,
+  residential_type      VARCHAR2(10) CHECK( residential_type IN ('male','female') ) NOT NULL,
   residential_name      VARCHAR(40) NOT NULL
 );
 
 CREATE TABLE student_info(
-  student_roll          NUMBER(10)   NOT NULL,
-  registration_id       NUMBER(10)   NOT NULL,   
+  student_roll          VARCHAR(10)   NOT NULL,
+  registration_id       VARCHAR(10)   NOT NULL,   
   student_first_name    VARCHAR(40)  NOT NULL,
   student_second_name   VARCHAR(40),  
   student_father_name   VARCHAR(40)  NOT NULL,
   student_mother_name   VARCHAR(40)  NOT NULL,
-  student_phone_number  NUMBER(15)   NOT NULL,
+  student_phone_number  VARCHAR(15)   NOT NULL,
   date_of_birth         DATE         NOT NULL,
-  gender                VARCHAR(10)  NOT NULL,
+  gender                VARCHAR2(10) CHECK( gender IN ('male','female') ) NOT NULL,
   religion              VARCHAR(20),
   permanent_address     VARCHAR(60),
   present_address       VARCHAR(60),
   student_email         VARCHAR(40),
   nationality           VARCHAR(30),
-  status                VARCHAR(40)  
+  status                VARCHAR(40),
+  session_id            VARCHAR(10),
+  session_id_origin     VARCHAR(10),
+  residential_id        VARCHAR(10)
+
 );
 
 CREATE TABLE courses(
-  course_id        NUMBER(10)   NOT NULL,
-  course_code      NUMBER(10)   NOT NULL,
+  course_id        VARCHAR(10)   NOT NULL,
+  course_code      VARCHAR(10)   NOT NULL,
   course_title     VARCHAR(40)  NOT NULL,
-  course_credit    NUMBER(3)    NOT NULL,
-  course_type      VARCHAR(20)  NOT NULL  
+  course_credit    FLOAT(5)    NOT NULL,
+  course_type      VARCHAR2(30) CHECK( course_type IN ('lab','theory','central viva', 'tour') ) NOT NULL,
+  session_id       VARCHAR(10),
+  semester_id      VARCHAR(10),
+  teacher_id       VARCHAR(10)
+    
 );
 
 CREATE TABLE grade_scale(
-  upper_limit      NUMBER(4)    NOT NULL,
-  lower_limit      NUMBER(4)    NOT NULL,
-  gp               NUMBER(5)    NOT NULL,
-  letter_grade     NUMBER(5)    NOT NULL 
+  upper_limit      FLOAT(5)    NOT NULL,
+  lower_limit      FLOAT(5)    NOT NULL,
+  gp               FLOAT(5)    NOT NULL,
+  letter_grade     VARCHAR(2)    NOT NULL 
 );
 
 CREATE TABLE promotion(
-  promotion_id        NUMBER(10)   NOT NULL
+  promotion_id        NUMBER(10)   NOT NULL,
+  semester_id         VARCHAR(10),
+  session_id          VARCHAR(10)
+
 );
 
 CREATE TABLE marks_theory(
-  third_examiner        NUMBER(4),
-  external_examiner     NUMBER(4),  
-  internal_examiner     NUMBER(4)   NOT NULL,
-  class_test            NUMBER(4)   NOT NULL,
-  attendance            NUMBER(4)   NOT NULL
+  third_examiner        FLOAT(5),
+  external_examiner     FLOAT(5),  
+  internal_examiner     FLOAT(5),
+  class_test            FLOAT(5),
+  attendance            FLOAT(5),
+  student_roll          VARCHAR(10),
+  course_id        VARCHAR(10)
 );
 
 CREATE TABLE marks_lab(
-  cpa    NUMBER(4)   NOT NULL,
-  qv     NUMBER(4)   NOT NULL,  
-  pr     NUMBER(4)   NOT NULL
+  cpa    FLOAT(5),
+  qv     FLOAT(5),  
+  pr     FLOAT(5),
+  student_roll          VARCHAR(10),
+  course_id        VARCHAR(10)
 );
 
 CREATE TABLE marks_viva(
-  total    NUMBER(4)   NOT NULL
+  total    FLOAT(4),
+  student_roll          VARCHAR(10),
+  course_id        VARCHAR(10)
 
 );
 
 CREATE TABLE marks_thesis_or_project(
-  third_examiner        NUMBER(4),
-  external_examiner     NUMBER(4),  
-  internal_examiner     NUMBER(4)   NOT NULL,
-  pr                    NUMBER(4)   NOT NULL,
-  viva_voce             NUMBER(4)   NOT NULL
+  third_examiner        FLOAT(5),
+  external_examiner     FLOAT(5),  
+  internal_examiner     FLOAT(5),
+  pr                    FLOAT(5),
+  viva_voce             FLOAT(5),
+  student_roll          VARCHAR(10),
+  course_id             VARCHAR(10)
 );
 
 CREATE TABLE teacher(
-  teacher_id          NUMBER(10)  NOT NULL,
+  teacher_id          VARCHAR(10)  NOT NULL,
   teacher_first_name  VARCHAR(30) NOT NULL,
   teacher_second_name VARCHAR(30) NOT NULL,
-  teacher_designation VARCHAR(30) NOT NULL
+  teacher_designation VARCHAR(30) NOT NULL,
+  department_id      VARCHAR(2)
 );
 
 
